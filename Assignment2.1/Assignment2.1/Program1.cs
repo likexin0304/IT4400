ca56e559e947e833beed605c36434d2d4a4c2e22
KEXIN LI <danielleeprince@gmail.com>
2017-06-20 13:15:27 +0800

    Add files via upload

---
+++ Assignment2.1/Assignment2.1/Program1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._1
+{
+    // struct Date incluing year, month, and day. All are int type
+    struct Date
+    {
+        enum Months { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+        public int year, month, day;
+
+        // two consructors:
+        // public Date (int year, int month, int day)
+        public Date(int year, int month, int day)
+        {
+            this.year = year; // this.year is field 
+            this.month = month;
+            //this.month = Convert.ToString(month);
+            this.day = day;
+        }
+        ////public Date(string dateString).
+        //public Date(string dateString)
+        //{
+        //    //mm/dd/yyyy
+        //    string olddate = Console.ReadLine();
+        //    dateString = olddate;
+        //    //int olddate1 = Int32.Parse(olddate);
+        //    string[] newdate = dateString.Split('/'); //get every data
+        //    int[] newdate1 = new int[newdate.Length];
+        //    for (int i = 0; i < newdate.Length; i++)
+        //    {
+        //        newdate1[i] = int.Parse(newdate[i]);
+        //    }
+
+        //    this.month = newdate1[0];
+        //    this.day = newdate1[1];
+        //    this.year = newdate1[2];
+
+        //}
+        // print the date
+        public void printInformation()
+        {
+            // Console.WriteLine("Year: {0}", year);
+            //Console.WriteLine("Month: {0}", month);
+            //Console.WriteLine("Date: {0}", day);
+            string month1 = "";
+
+            if (this.month == 01)
+            {
+                month1 = Date.Months.Jan.ToString();
+            }
+            if (this.month == 02)
+            {
+                month1 = Date.Months.Feb.ToString();
+            }
+            if (this.month == 03)
+            {
+                month1 = Date.Months.Mar.ToString();
+            }
+            if (this.month == 04)
+            {
+                month1 = Date.Months.Apr.ToString();
+            }
+            if (this.month == 05)
+            {
+                month1 = Date.Months.May.ToString();
+            }
+            if (this.month == 06)
+            {
+                month1 = Date.Months.Jun.ToString();
+            }
+            if (this.month == 07)
+            {
+                month1 = Date.Months.Jul.ToString();
+            }
+            if (this.month == 08)
+            {
+                month1 = Date.Months.Aug.ToString();
+            }
+            if (this.month == 09)
+            {
+                month1 = Date.Months.Sep.ToString();
+            }
+            if (this.month == 10)
+            {
+                month1 = Date.Months.Oct.ToString();
+            }
+            if (this.month == 11)
+            {
+                month1 = Date.Months.Nov.ToString();
+            }
+            if (this.month == 12)
+            {
+                month1 = Date.Months.Dec.ToString();
+            }
+
+
+            Console.WriteLine("Today is {0}, {1}, {2}.\n ", month1, day, year);
+
+
+        }
+    }
+
+
+
+    class Program1
+    {
+        //enum Months { Jan =1, Feb, mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+        static void Main(string[] args)
+        {
+            Date d = new Date();
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                string olddate = Console.ReadLine();
+                //int olddate1 = Int32.Parse(olddate);
+                string[] newdate = olddate.Split(' '); //get every data
+                int[] newdate1 = new int[newdate.Length];
+                for (int i = 0; i < newdate.Length; i++)
+                {
+                    newdate1[i] = int.Parse(newdate[i]);
+                }
+                //d.Date(newdate1[0], newdate1[1],newdate1[2]); //get the year from user
+                d.year = newdate1[0];
+                d.month = newdate1[1];
+                d.day = newdate1[2];
+                d.printInformation();
+                //Console.WriteLine("You chose 1.");
+            }
+            if (choice == 2)
+            {
+                string olddate = Console.ReadLine();
+                //dateString = olddate;
+                //int olddate1 = Int32.Parse(olddate);
+                string[] newdate = olddate.Split('/'); //get every data
+                int[] newdate1 = new int[newdate.Length];
+                for (int i = 0; i < newdate.Length; i++)
+                {
+                    newdate1[i] = int.Parse(newdate[i]);
+                }
+                d.month = newdate1[0];
+                d.day = newdate1[1];
+                d.year = newdate1[2];
+                d.printInformation();
+               // Console.WriteLine("You chose 2.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment2.2/Assignment2.2/Program2.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._2
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private double price;
+        private string isbn;
+        public Book(string title, string author, double price, string isbn)
+        {
+            
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            this.isbn = isbn;
+            
+           // printInfomation();
+        }
+        
+        public void printInfomation()//Book[] bookss
+        {
+            //“title written by author is price, with ISBN isbn”
+           //foreach (var title in bookss)
+
+                Console.WriteLine("{0} written by {1} is {2} dollars, with ISBN {3}\n", title, author, price, isbn);
+        }
+        public string Title
+        {
+            // get accessor is read
+            //set accessor is write
+            get
+            {
+                return this.title;
+
+            }
+            
+
+        }
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+        }
+        public string ISBN
+        {
+            get
+            {
+                return this.isbn;
+            }
+        }
+        public static int CompareByPrice(Book book1, Book book2)
+        {
+            //return city1.Population.CompareTo(city2.Population);
+            return book1.Price.CompareTo(book2.Price);
+        }
+        public static int CompareBytitle(Book book1, Book book2)
+        {
+            //return String.Compare(city1.Country + city1.City, city2.Country + city2.City);
+            return String.Compare(book1.title,book2.title);
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            string readdate = Console.ReadLine();
+            //int olddate1 = Int32.Parse(olddate);
+            string[] newdate = readdate.Split(' '); //get every data
+            // get the number of books
+            int books = Convert.ToInt32(newdate[0]);
+			// get the choice 1 or 2
+            int choice = Convert.ToInt32(newdate[1]);
+			// create arry for store all data
+            string[] title = new string[books];
+            string[] author = new string[books];
+            string[] rawprice = new string[books];
+            double[] price = new double[books];
+            string[] isbn = new string[books];
+            Book[] bookss = new Book[books];
+            //Book[] bookarray = new Book(title, author,price,isbn);
+            if (choice == 1)
+            {
+                for ( int i = 0; i < books; i++)
+                {
+                    title[i] = Console.ReadLine();
+                    author[i] = Console.ReadLine();
+                    rawprice[i] = Console.ReadLine();
+                    price[i] = Convert.ToDouble(rawprice[i]);
+                    isbn[i] = Console.ReadLine();
+
+                 }
+                
+
+                for( int i =0; i <books; i++)
+                {
+                    //Book booktemp = new Book(title[i], author[i], price[i], isbn[i]);
+                    bookss[i] = new Book(title[i], author[i], price[i], isbn[i]);
+                    //Array.Sort(bookss, Book.CompareByPrice);
+                    //printInfomation(bookss);
+                   // Book booktemp = new Book(title[i], author[i], price[i], isbn[i]);
+                }
+                Array.Sort(bookss, Book.CompareByPrice);
+                for(int i = 0; i < books; i++)
+                {
+                    bookss[i].printInfomation();
+                }
+                //for (int i = 0; i< books; i++)
+                //{
+                //    Book booktemp = new Book(title[i], author[i], price[i], isbn[i]);
+                //}
+                // for( int i = 0; i < books; i++)
+                //{
+                //    Console.WriteLine("{0}", title[i]);
+                //    Console.WriteLine("{0}", author[i]);
+                //    Console.WriteLine("{0}", price[i]);
+                //    Console.WriteLine("{0}", isbn[i]);
+                //}
+
+
+            }
+            if (choice == 2)
+            {
+               
+                string titletemp = string.Empty;
+                for (int i = 0; i < books; i++)
+                {
+                    title[i] = Console.ReadLine();
+                    author[i] = Console.ReadLine();
+                    rawprice[i] = Console.ReadLine();
+                    price[i] = Convert.ToDouble(rawprice[i]);
+                    isbn[i] = Console.ReadLine();
+                }
+                for (int i = 0; i < books; i++)
+                {
+                    //Book booktemp = new Book(title[i], author[i], price[i], isbn[i]);
+                    bookss[i] = new Book(title[i], author[i], price[i], isbn[i]);
+                   // Array.Sort(bookss, Book.CompareBytitle);
+                    //printInfomation(bookss);
+                    // Book booktemp = new Book(title[i], author[i], price[i], isbn[i]);
+                }
+                Array.Sort(bookss, Book.CompareBytitle);
+                for (int i = 0; i < books; i++)
+                {
+                    bookss[i].printInfomation();
+                }
+
+
+
+
+
+
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
